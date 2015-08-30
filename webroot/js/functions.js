0b90d556b1f159fdd2e5b2a68d176020cdc510de
gbonos <gmponos@gmail.com>
2015-08-30 19:33:32 +0300

    Added a vendor folder in webroot for dependencies

--- webroot/js/functions.js
+++ webroot/js/functions.js
@@ -1,11 +1,11 @@
-$(document).on('ready', function() {
-    $('.form-control-chosen').each(function() {
-	$(this).chosen({
-	    placeholder_text: 'Select an option',
-	    placeholder_text_multiple: ' ',
-	    width: "100%",
-	    allow_single_deselect: true
-	});
+$(document).on('ready', function () {
+    $('.form-control-chosen').each(function () {
+        $(this).chosen({
+            placeholder_text: 'Select an option',
+            placeholder_text_multiple: ' ',
+            width: "100%",
+            allow_single_deselect: true
+        });
     });
 });
 
