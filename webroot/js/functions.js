7979a8ba0c17453d05dd4284a8157a5f1d65c9bc
gmponos <gmponos@gmail.com>
2015-10-17 13:09:27 +0300

    Added functions for various js

--- webroot/js/functions.js
+++ webroot/js/functions.js
@@ -15,7 +15,21 @@
  */
 
 $(document).on('ready', function () {
+
+    $('.form-control-chosen').each(function () {
+        $(this).chosen({
+            placeholder_text: 'Select an option',
+            placeholder_text_multiple: ' ',
+            width: "100%",
+            allow_single_deselect: true
+        });
+    });
+
     $('.form-control-datepicker').datepicker({
         format: "yyyy-mm-dd"
+    });
+
+    $('.form-control-clockpicker').clockpicker({
+        donetext: "Done."
     });
 });
