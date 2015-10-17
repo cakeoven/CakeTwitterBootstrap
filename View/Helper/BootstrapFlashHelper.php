fda3251be05b0c32b4e01566aaa04e3028dca5d2
gmponos <gmponos@gmail.com>
2015-10-17 13:48:44 +0300

    Added the Flash Helper and elements needed

---
+++ View/Helper/BootstrapFlashHelper.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Copyright 2014, George Mponos
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright 2014, George Mponos
+ * @author    George Mponos, <gmponos@gmail.com>
+ * @link      http://github.com/gmponos/CakeTwitterBootstrap
+ * @license   MIT License (http://www.opensource.org/licenses/mit-license.php)
+ *            horizontal elements must be created with an option
+ */
+App::uses('FlashHelper', 'View/Helper');
+
+/**
+ * Bootstrap Flash Helper
+ * A helper to create forms compatibles
+ * with the bootstrap front end framework
+ */
+class BootstrapFlashHelper extends FlashHelper
+{
+    
+}
